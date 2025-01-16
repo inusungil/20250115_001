ea1000e9f4fd06236201ee068b5e2cc62ec0fc06
inusungil <inusungil@hanmail.net>
2025-01-16 20:24:46 +0900

    Initial commit 태스트용 print 추가

---
+++ 20250115_001.py
@@ -0,0 +1,35 @@
+## 이교진교수님
+## https://github.com/gyojincompany/git_github
+## https://git-scm.com/downloads
+## 64-bit Git for windows 다운로드
+## Git Bash
+
+## github.com 가입후 Create your free account
+## Sign up to GitHub- 이메일 패스워드 유저네임  - 이메일로 보안코드 전송
+
+## public  저장소마다 주소 받음
+## private  초대한 사람만 볼수 있음
+
+## $git config --global user.name "계정이름"
+## $git config --global user.email inusungil@hanmail.net
+
+## 구글독스 google docs
+
+## https://github.com/inusungil/sik20250115.git
+
+## 토큰발급
+## 설정  - 버전컨트롤 - git hub - 클릭 (-) - 클릭(+) -
+#  log with token - 키복붙 - add acount
+
+## computer1  - commit push
+
+## repository -  computer2 - clone
+# 업데이트 되면 fetch 확인만 함
+# pull 업데이트함 다운로드
+
+## clone - fetch - pull
+
+print("test01")
+
+# 버전컨트롤 vcs  share project on - git - project name
+##
